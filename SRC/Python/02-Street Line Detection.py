23e8250c523c5596446012491da7010224bab05b
Farhan Daemi <fdmxfarhan@yahoo.com>
2024-03-15 00:05:59 +0330

    asdf

---
+++ SRC/Python/02-Street Line Detection.py
@@ -0,0 +1,50 @@
+import cv2 
+import numpy as np
+vid = cv2.VideoCapture(0) 
+  
+while(True): 
+    ret, image = vid.read() 
+    
+    converted_image = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
+    
+    #White color mask
+    lower_threshold = np.uint8([0, 200, 0])
+    upper_threshold = np.uint8([255, 255, 255])
+    white_mask = cv2.inRange(converted_image, lower_threshold, upper_threshold)
+    
+    #Yellow color mask
+    lower_threshold = np.uint8([10, 0, 100])
+    upper_threshold = np.uint8([40, 255, 255])
+    yellow_mask = cv2.inRange(converted_image, lower_threshold, upper_threshold)
+    
+    #Combine white and yellow masks
+    mask = cv2.bitwise_or(white_mask, yellow_mask)
+    masked_image = cv2.bitwise_and(image, image, mask = mask)
+
+    mask = np.zeros_like(image)   
+    #Defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(image.shape) > 2:
+        channel_count = image.shape[2]
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+    #We could have used fixed numbers as the vertices of the polygon,
+    #but they will not be applicable to images with different dimesnions.
+    rows, cols = image.shape[:2]
+    bottom_left  = [cols * 0.1, rows * 0.95]
+    top_left     = [cols * 0.4, rows * 0.6]
+    bottom_right = [cols * 0.9, rows * 0.95]
+    top_right    = [cols * 0.6, rows * 0.6]
+    vertices = np.array([[bottom_left, top_left, top_right, bottom_right]], dtype=np.int32)
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    masked_image1 = cv2.bitwise_and(image, mask)
+
+
+
+    cv2.imshow('image', image) 
+    cv2.imshow('masked_image', masked_image) 
+    if cv2.waitKey(1) & 0xFF == 27: 
+        break
+  
+vid.release() 
+cv2.destroyAllWindows() 
